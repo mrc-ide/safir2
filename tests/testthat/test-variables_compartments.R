16d4735f57303aec1e12afc57fdea356115478c7
slwu89 <slwu89@users.noreply.github.com>
2022-01-03 21:11:25 -0800

    create age/compartment vars

---
+++ tests/testthat/test-variables_compartments.R
@@ -0,0 +1,11 @@
+test_that("create_variables returns the correct output", {
+
+  pop <- get_population("AFG")
+
+  pop$n <- as.integer(pop$n/10000)
+  theages <- create_variables(pop, get_squire_parameters("AFG", dt = 1))
+  expect_length(length(theages$discrete_age), 1)
+
+  expect_length(theages$discrete_age$get_values(), sum(pop$n))
+
+})
